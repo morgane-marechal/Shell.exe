a81dfa62fe5f460364d3ec9d2e52396fc9b58fc1
morgane <morgane@debian>
2022-10-06 15:42:47 +0200

    correction job09

--- Job09/accessrights.sh
+++ Job09/accessrights.sh
@@ -14,15 +14,13 @@ do
         echo "Role : $role"
 sudo useradd -m $nom
 echo $nom:$mdp | sudo chpasswd
-done < $INPUT
 
-while read id nom prenom mdp role
-do
-echo "$role"
-if [ $role == "Admin" ]
+if [ ${role:0:5} = "Admin" ]
 then
 sudo usermod -aG sudo $nom
-echo "$nom  ajouter au groupe sudo"
+echo "$nom ajouter au groupe sudo"
+#sudo deluser $nom sudo
 fi
 done < $INPUT
+
 
